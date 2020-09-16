cece1c7b5b8484dda8211ed3102fbd211dd6da5b
AaravSrivastav <22asrivastav@athenian.org>
2020-09-16 00:54:20 +0000

    added a binaryToDecimal method to main

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,23 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+        int val = 0;
+        for(int i = 0; i<binary.length(); i++)
+        {
+            if(binary.charAt(binary.length()-i-1) == '1')
+            {
+                val += Math.pow(2, i);
+            }
+        }
+        return val;
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Type in a number in binary format: ");
+        String binary = scan.next();
+        System.out.println("That is equal to the decimal value: " + binaryToDecimal(binary));
+        scan.close();
     }
 }
